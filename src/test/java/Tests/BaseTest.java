7d0a1634dfad6f1e4127abf0e213f96be76c66ba
Nadia Nikolaeva <44nikolaeva@mail.ru>
2022-04-19 16:29:07 +0300

    Added SettingsTest.class but it still working unstable

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -6,5 +6,5 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({Tests.class})
+@SelectClasses({Tests.class, SettingsTest.class})
 public class BaseTest {}
